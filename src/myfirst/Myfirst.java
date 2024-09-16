34e5176b68368e2e5bc89e71d55f86ce9654bb60
panchojo <panchojo@sheridancollege.ca>
2024-09-16 12:57:14 -0400

    Save

---
+++ src/myfirst/Myfirst.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myfirst;
+
+/**
+ *
+ * @author Joy
+ * @modified by me 
+ */
+public class Myfirst {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        // TODO code application logic here
+    }
+    
+}
